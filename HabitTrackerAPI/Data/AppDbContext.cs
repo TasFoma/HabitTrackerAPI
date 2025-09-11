e8a01849195e7e25f5e95d02973077e379a5c97a
TasFoma <17tasmanskiy12foma@mail.ru>
2025-09-11 22:20:59 +0700

    setting up the background

--- HabitTrackerAPI/Data/AppDbContext.cs
+++ HabitTrackerAPI/Data/AppDbContext.cs
@@ -4,9 +4,7 @@ namespace HabitTrackerAPI.Models;
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-    {
-    }
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)  { }
 
     public DbSet<Habit> Habits { get; set; } // Создает таблицу Habits в БД
 }
